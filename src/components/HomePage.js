763582da0e1e5280aceae7fe1f5c23383cc8f43a
atsunorium <65150911+atsunorium@users.noreply.github.com>
2022-07-13 17:55:26 +0900

    up

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import illustratorImage from "../Images/illustrator.svg";
 import jsImage from "../Images/js.png";
 import phpImage from "../Images/php.svg";
